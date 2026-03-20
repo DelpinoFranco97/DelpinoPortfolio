44b8853ae02c3029b987f42e30d04b601c38b9d8
delpinofranco <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>
2026-03-20 18:47:09 -0300

    menu responsive finalizado

--- javascript.js
+++ javascript.js
@@ -1,7 +1,51 @@
 const btnHambur = document.getElementById("hambur")
+const menuresposive = document.getElementById("menuresponsive")
+const btnX = document.getElementById("x")
 
+btnX.addEventListener("click",()=>{
+    const estado = window.getComputedStyle(menuresposive).display;
 
+     if (estado !== "none"){
+        btnX.style.display = "none";
+        menuresposive.style.display = "none"
+        btnHambur.style.display = "block";
+
+     }
+       
+    //  else{
+    //     menuresposive.style.display = "none"
+    //     btnHambur.style.display = "block";
+    //     btnX.style.display = "display";
+
+        
+    //  }
+
+})
 btnHambur.addEventListener("click",()=>{
+    const estado = window.getComputedStyle(menuresposive).display;
+    if(estado === "none")
+        {
+           
+            menuresposive.style.display = "block";
+            menuresposive.style.width = "100%";
+            btnHambur.style.display = "none";
+            btnX.style.display = "block"
+            // menuresposive.style.display = "none";
+        }
+
+
+
+    // else 
+    //     {
+    //         menuresposive.style.display = "none"
+    //         btnHambur.style.display = "none";
+            
+            
+
+    //         // menuresposive.style.display = "block";
+    //         // menuresposive.style.width = "100%";
+    
+    //     }
 
     
 })

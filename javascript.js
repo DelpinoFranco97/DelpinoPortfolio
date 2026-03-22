1b31881d871d683912287f48548d46a74444724e
delpinofranco <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>
2026-03-22 15:28:56 -0300

    Configuracion de botones para entrar a las redes sociales y correcion de bug del nav responsive

--- javascript.js
+++ javascript.js
@@ -1,51 +1,32 @@
-const btnHambur = document.getElementById("hambur")
-const menuresposive = document.getElementById("menuresponsive")
-const btnX = document.getElementById("x")
+const btnHambur = document.getElementById("hambur");
+const menuresposive = document.getElementById("menuresponsive");
+const nav = document.getElementById("navResponsive");
+const btnX = document.getElementById("x");
 
-btnX.addEventListener("click",()=>{
+btnX.addEventListener("click", () => {
     const estado = window.getComputedStyle(menuresposive).display;
 
-     if (estado !== "none"){
+    if (estado !== "none") {
         btnX.style.display = "none";
-        menuresposive.style.display = "none"
+        menuresposive.style.display = "none";
         btnHambur.style.display = "block";
-
-     }
-       
-    //  else{
-    //     menuresposive.style.display = "none"
-    //     btnHambur.style.display = "block";
-    //     btnX.style.display = "display";
-
+        nav.style.display = "flex";
         
-    //  }
+    }
+});
 
-})
-btnHambur.addEventListener("click",()=>{
+btnHambur.addEventListener("click", () => {
     const estado = window.getComputedStyle(menuresposive).display;
-    if(estado === "none")
-        {
-           
-            menuresposive.style.display = "block";
-            menuresposive.style.width = "100%";
-            btnHambur.style.display = "none";
-            btnX.style.display = "block"
-            // menuresposive.style.display = "none";
-        }
 
-
-
-    // else 
-    //     {
-    //         menuresposive.style.display = "none"
-    //         btnHambur.style.display = "none";
-            
-            
-
-    //         // menuresposive.style.display = "block";
-    //         // menuresposive.style.width = "100%";
-    
-    //     }
-
-    
-})
+    if (estado === "none") {
+        menuresposive.style.display = "block";
+        menuresposive.style.width = "100%";
+        btnHambur.style.display = "none";
+        btnX.style.display = "block";
+        nav.style.display = "none";
+        menuresposive.style.position="fixed";
+        menuresposive.style.top = "0";
+menuresposive.style.left = "0";
+menuresposive.style.height = "auto";
+    }
+});
